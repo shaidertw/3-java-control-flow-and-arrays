a2d25a8a75df80c7a6a84b77f0ee0a5fa7fd6152
shaidermeister <shaidermeister@gmail.com>
2021-09-25 16:00:50 +0500

    init commit

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -2,15 +2,13 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getMin(5,4,3));
-         */
+        System.out.println(getMin(5,-1,3));
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min;
+        min = Math.min(a, b);
+        min = Math.min(min, c);
+        return min;
     }
 }
