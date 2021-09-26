8c26108d7a25d126c905543125a911a650c595de
shaidermeister <shaidermeister@gmail.com>
2021-09-26 20:06:45 +0500

    init commit

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,17 +12,22 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
+
+        if(arr==null || arr.length==0) {
+            return;
+        }
+
         int minIndex=0;
         int tmpValue;
         int minValue=Integer.MAX_VALUE;
         for(int i=0;i<arr.length;++i){
-            if(arr[i]<minValue){
+            if(arr[i]<=minValue){
                 minValue=arr[i];
                 minIndex=i;
             }
         }
         tmpValue=arr[0];
-        arr[0]=arr[minIndex];
+        arr[0]=minValue;
         arr[minIndex]=tmpValue;
     }
 
